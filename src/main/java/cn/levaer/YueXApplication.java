2a60d4465b988489f2eb11518689abc5be04c736
Leader <LM20070403@gmail.com>
2026-08-29 10:50:57 +0800

    Initial commit

---
+++ src/main/java/cn/levaer/YueXApplication.java
@@ -0,0 +1,13 @@
+package cn.levaer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YueXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YueXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/levaer/YueXApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.levaer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YueXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
