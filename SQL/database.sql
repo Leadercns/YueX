d7f388dc13a46ac7402a1eff414b1aa43286764e
Leader <206212893+Leadercns@users.noreply.github.com>
2026-09-01 15:29:04 +0800

    add:下属用户登录/注册     获取所有用户列表     获取指定用户信息     封禁用户     用户签到     重置开发者ID fix:updete database.sqlSQL upload time:2026年9月1日15:23:14(UTC+8 BEIJING) Remarks:整个代码就写到这，有问题请在github上面指出问题，谢谢。

--- SQL/database.sql
+++ SQL/database.sql
@@ -1,1 +1,49 @@
-1
+create table admins
+(
+    username  varchar(18)                  not null comment '管理员名称',
+    password  varchar(255)                 not null comment '管理员密码',
+    adminid   char(15)                     not null comment '管理员ID',
+    ICode     char(10)                     not null comment '邀请码',
+    ICodeS    tinyint unsigned default '0' null comment '邀请码是否使用 1为是 0为否',
+    State     tinyint unsigned default '0' not null comment '状态 0正常 1封禁',
+    token     char(20)                     null comment '管理员登录注释',
+    tokentime datetime                     null comment 'token生成时间',
+    constraint ICode
+        unique (ICode),
+    constraint adminid
+        unique (adminid),
+    constraint token
+        unique (token),
+    constraint username
+        unique (username)
+);
+
+create table developer
+(
+    username        varchar(18)                  not null comment '开发者账号',
+    password        varchar(255)                 not null comment '开发者密码',
+    id              char(18)                     not null comment '开发者ID',
+    Security_answer varchar(255)                 not null comment '开发者约定的密保答案',
+    login_ip        varchar(17)                  null comment '登录IP',
+    State           tinyint unsigned default '0' not null comment '开发者状态 0正常 1封禁',
+    constraint id
+        unique (id),
+    constraint username
+        unique (username)
+)
+    comment '开发者表';
+
+create table users
+(
+    deveid    char(18)                     not null comment '开发者ID',
+    username  varchar(18)                  not null comment '用户名',
+    password  varchar(255)                 not null comment '密码',
+    Points    int unsigned     default '0' null comment '用户积分',
+    checktime datetime                     null comment '签到时间',
+    State     tinyint unsigned default '0' not null comment '用户状态 0正常 1封禁'
+)
+    comment '开发者下属用户表';
+
+create index userid
+    on users (deveid);
+
